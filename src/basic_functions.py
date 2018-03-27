a0d858831f8d962054bc3753d0c26524ae9b1cd3
burchill <zach.burchill+gh@gmail.com>
2018-03-27 00:11:39 -0400

    moved v0.9 ninja funcs to basic_funcs.py

--- src/basic_functions.py
+++ src/basic_functions.py
@@ -4,8 +4,9 @@ Created on Mar 16, 2018
 @author: zburchill
 '''
 import urllib.parse
-import logging, os
+import logging, os, random, functools
 import io # for loading images
+from warnings import warn
 
 ### TO-DO: make sure all the PageScrapeExceptions in the code have the new arugments
 
@@ -69,23 +70,7 @@ def load_image(image_url, *args, **kwargs):
     im = Image.open(image_file)
     return(im)
 
-# inspired by: 'https://codelike.pro/create-a-crawler-with-rotating-ip-proxy-in-python/'
-def get_proxies(**kwargs):
-    """ 
-    Returns a list of dicts of ssl proxies from sslproxies.org
-    """
-    proxies = []
-    proxy_url='https://www.sslproxies.org/'
-    soup = soupify(proxy_url, **kwargs)
-    for row in soup.tbody.find_all("tr"):
-        proxies.append({
-            'ip':   row.find_all('td')[0].string,
-            'port': row.find_all('td')[1].string
-        })
-    return(proxies)
-
-
-# ---------------------------------- soup functions ----------------------------------------
+# ---------------------------------- soup functions ---------------------------------------- #
 
 def soupify(url_t, safer=False, **kwargs):
     """ Returns a bs4 soup object from a url """
@@ -113,6 +98,151 @@ def clean_find_all(soup_t,args):
         return(soup_t.find_all(args))
     else:
         return(soup_t.find_all(*args))
+    
+    
+# ---------------------------------- 'Ninja' functions ---------------------------------------- #
+# These functions are built to basically sneak around websites that detect and block web-scraping
+# There are two ways I made this work: 
+#    The first is through more extendable decorators:`proxy_decorator()` and `manage_proxies()`. 
+#        Examples for how to use those are shown farther below.
+#    The second is just a prebuilt class called `ninja_soupify_simpler()` which is a less complicated,
+#        but more rigid version.
+
+def proxy_decorator(switch_after_n):
+    """ A decorator that let's you specify the number of requests before 
+    switching proxies for `manage_proxies` and keeps the decorator syntactic sugar."""
+    def inner_decorator(fn):
+        functools.wraps(fn)
+        return(manage_proxies(fn, switch_after_n))
+    return inner_decorator
+
+class manage_proxies(object):
+    """ A class meant to store the proxy information for a function.
+    
+    If you want that function's docs, use `<that function>.__doc__`. """
+    
+    # To make this class a decorator itself, just remove `switch_proxies_after_n_calls` from `__init__`
+    #    (maybe add it in via as a default arg to `f`) 
+    def __init__(self, f, switch_proxies_after_n_calls):
+        self.__f = f
+        self._switch_after_n = switch_proxies_after_n_calls
+        self.__numcalls = self.proxy_index = 0
+        self.proxies = self.get_proxies()
+        functools.update_wrapper(self, f) #
+    
+    def __call__(self, *args, **kwargs):
+        self.__numcalls += 1
+        if self.__numcalls % self._switch_after_n == 0:
+            self.proxy_index = self.get_random_proxy_index()
+        return self.__f(*args, **kwargs)
+    
+    def count(self):
+        "Return the number of times the function f was called."
+        return self.__numcalls
+    
+    def get_random_proxy_index(self):
+        "Return a random index from the proxy list"
+        return random.randint(0, len(self.proxies) - 1)
+    
+    # inspired by: 'https://codelike.pro/create-a-crawler-with-rotating-ip-proxy-in-python/'
+    @staticmethod
+    def get_proxies(**kwargs):
+        """ 
+        Returns a list of dicts of ssl proxies from sslproxies.org
+        """
+        proxies = []
+        proxy_url = 'https://www.sslproxies.org/'
+        soup = soupify(proxy_url, **kwargs)
+        for row in soup.tbody.find_all("tr"):
+            proxies.append({
+                'ip':   row.find_all('td')[0].string,
+                'port': row.find_all('td')[1].string
+            })
+        return(proxies)
+  
+# An example of how to use `proxy_decorator()` and `manage_proxies()`
+# # Only does https currently
+# @proxy_decorator(SWITCH_PROXIES_AFTER_N_REQUESTS)
+# def ninja_soupify(url, tolerance=10, **kwargs):
+#     dead_proxy_count = 0
+#     # if you burn through too many proxies, there's probably a problem
+#     while dead_proxy_count <= tolerance:
+#         # If for some reason there isn't a proxies list, make one
+#         if not ninja_soupify.proxies: 
+#             ninja_soupify.proxies = ninja_soupify.get_proxies()
+#         # If the proxy index needs to be updated
+#         if ninja_soupify.proxy_index >= len(ninja_soupify.proxies):
+#             ninja_soupify.proxy_index = ninja_soupify.get_random_proxy_index()
+#         # Make a dict that Requests can use
+#         proxy = { "https": "http://{ip}:{port}".format(**ninja_soupify.proxies[ninja_soupify.proxy_index]) }
+#         try:
+#             r = soupify(url, proxies = proxy, **kwargs)
+#             return(r)
+#         # If the proxy doesn't work:
+#         except (requests.exceptions.SSLError, requests.exceptions.ProxyError) as err:
+#             warn("Proxy {d[ip]}:{d[port]} deleted because of: {error_m!s}".format(d = ninja_soupify.proxies[ninja_soupify.proxy_index], error_m=err))
+#             del ninja_soupify.proxies[ninja_soupify.proxy_index]
+#             dead_proxy_count += 1
+#     raise PageScrapeException(url=url, message="Burned through too many proxies ({!s})".format(tolerance))
+         
+class ninja_soupify_simpler(object):
+    """ A simpler, more rigid version of what `proxy_decorator` leads into.
+    This class basically serves as a function that stores proxy information and rotates
+    through using different ones while using `soupify()`. """
+    
+    def __init__(self, switch_proxies_after_n_calls):
+        self.switch_after_n = switch_proxies_after_n_calls
+        self.numcalls = self.proxy_index = 0
+        self.proxies = self.get_proxies()
+        
+    def __call__(self, url, tolerance=10, **kwargs):
+        self.numcalls += 1
+        if self.numcalls % self.switch_after_n == 0:
+            self.proxy_index = self.get_random_proxy_index()        
+        dead_proxy_count = 0
+        # if you burn through too many proxies, there's probably a problem
+        while dead_proxy_count <= tolerance:
+            # If for some reason there isn't a proxies list, make one
+            if not self.proxies: 
+                self.proxies = self.get_proxies()
+            # If the proxy index needs to be updated
+            if self.proxy_index >= len(self.proxies):
+                self.proxy_index = self.get_random_proxy_index()
+            # Make a dict that Requests can use
+            proxy = { "https": "http://{ip}:{port}".format(**self.proxies[self.proxy_index]) }
+            try:
+                r = soupify(url, proxies = proxy, **kwargs)
+                return(r)
+            # If the proxy doesn't work:
+            except (requests.exceptions.SSLError, requests.exceptions.ProxyError) as err:
+                warn("Proxy {d[ip]}:{d[port]} deleted because of: {error_m!s}".format(d = self.proxies[self.proxy_index], error_m=err))
+                del self.proxies[self.proxy_index]
+                dead_proxy_count += 1
+        raise PageScrapeException(url=url, message="Burned through too many proxies ({!s})".format(tolerance))
+        
+    def get_random_proxy_index(self):
+        "Return a random index from the proxy list"
+        return random.randint(0, len(self.proxies) - 1)
+    
+    # inspired by: 'https://codelike.pro/create-a-crawler-with-rotating-ip-proxy-in-python/'
+    @staticmethod
+    def get_proxies(**kwargs):
+        """ 
+        Returns a list of dicts of ssl proxies from sslproxies.org
+        """
+        proxies = []
+        proxy_url = 'https://www.sslproxies.org/'
+        soup = soupify(proxy_url, **kwargs)
+        for row in soup.tbody.find_all("tr"):
+            proxies.append({
+                'ip':   row.find_all('td')[0].string,
+                'port': row.find_all('td')[1].string
+            })
+        return(proxies)
+
+# Example:
+# ninja_soupify = ninja_soupify_simpler(SWITCH_PROXIES_AFTER_N_REQUESTS)
+# print(ninja_soupify("https://google.com"))
 
 
 
