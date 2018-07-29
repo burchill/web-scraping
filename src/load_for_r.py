c684ead73feb3135cd4006328b9f2269a18d9ce7
burchill <zach.burchill+gh@gmail.com>
2018-07-29 18:24:58 -0400

    added slimmed json and missing chapter finder

--- src/load_for_r.py
+++ src/load_for_r.py
@@ -4,12 +4,138 @@ Created on Apr 2, 2018
 @author: zburchill
 '''
 # 
-# from basic_functions import PersistentDict
+from basic_functions import PersistentDict
+from warnings import warn
+ 
+def load_dict(db_path):
+    db =  PersistentDict(db_path, flag='r', format="json")
+    d = db.to_dict()
+    return(d)
+
+def add_page(d, manga_id, page, k):
+    if manga_id in d:
+        if page in d[manga_id]:
+            print("{0} {1} already in dictionary!")
+            x=0/0
+        d[manga_id][page] = k
+    else:
+        d[manga_id] = {page: k}
+        
+    
+def clean_issues_dictionary(json_file, page_infix="_page_"):
+    d = load_dict(json_file)
+    max_pages =     [e for e in d.keys() if "pagenum" in e]
+    non_max_pages = [e for e in d.keys() if not "pagenum" in e]
+    set_ids = list(set([e.split("_")[0] for e in non_max_pages]))
+    
+    if len(set_ids) != len(max_pages):
+        warn("The number of unique series does not match the number of page entries")
+
+    used_series_ids = []
+    used_keys = []
+    max_page_dict = {}
+    missing_pages = []
+    good_dictionary = {}
+    
+    # Add the max pages to a dictionary
+    for i in max_pages:
+        manga_id = i.split("_")[0]
+        mp = d[i]
+        max_page_dict[manga_id] = mp
+    # Go through the dictionary:
+    for k, v in max_page_dict.items():
+        for page_number in range(1, v+1):
+            val = "{series_id}{infix}{n!s}".format(series_id = k,
+                                                   infix = page_infix,
+                                                   n =  page_number)
+            # This shouldn't happen
+            if val in used_keys:
+                print("oh shit!")
+                print(d[val])
+                x=0/0
+            
+            if val in non_max_pages:
+                used_keys += [val]
+                non_max_pages.remove(val)
+                add_page(good_dictionary, k, page_number, d[val])
+                
+            else:
+                missing_pages += [(k, page_number)]
+        used_series_ids += [k]
+    
+    print(len(non_max_pages))
+    print(non_max_pages)
+    print(missing_pages)
+    print(used_series_ids)
+    
+    return( (missing_pages, good_dictionary) )
+            
+
+missing_pages, good_d = clean_issues_dictionary("everything_json_issues.json") 
+print(len(missing_pages))
+
+print("AAA")
+print(good_d["33"][1])
+print(good_d["33"][1])
+    
+with PersistentDict("everything_json_issues_slimmed.json", 'c', format='json') as db:
+    for k, v in good_d.items():
+        db[k] = v
+
+    
+
+    
+#     
 # 
-# def load_dict(db_path, id_path):
-#     db =  PersistentDict(db_path, flag='r', format="json")
-#     d = db.to_dict()
-#     return(d)
+# 
+#    for page_number in range(1, v+1):
+#                 val = "{series_id}{infix}{n!s}".format(series_id = k,
+#                                                        infix = page_infix,
+#                                                        n =  page_number)
+#                 if val in db:
+#                     at_least_one = True
+#                     a = db[val]
+#                     if k in big_dict:
+#                         if page_number in big_dict[k]:
+#                             raise ValueError("Series number {!s}, page {!s} erroneously appears at least twice!".format(k, page_number))
+#                         else:
+#                             big_dict[k][page_number] = a
+#                     else:
+#                         big_dict[k] = {page_number: a}                       
+#                 else:
+#                     if k in dict_of_missing_chapters:
+#                         dict_of_missing_chapters[k] = dict_of_missing_chapters[k] + [page_number]
+#                     else:
+#                         dict_of_missing_chapters[k] = [page_number]
+#             if not at_least_one:
+#                 raise ValueError("Series number {!s} doesn't actually have any chapters!".format(k))
+#         
+#     
+# 
+# 
+# 
+# 
+# print(len(max_pages))
+# print(len(max_pages2))
+# print(len(d.keys()))
+# print(max_pages[1:10])
+# 
+# 
+# 
+# 
+# # 
+# # 
+# with shelve.open(db_path) as db:
+#     # Get all the series that have any issues recorded
+#     for k in ids:
+#         first_page_starter = str(k) + page_num_suffix
+#         if first_page_starter in db:
+#             max_pages = db[first_page_starter]
+#             if k in dict_of_all_saved_manga_ids:
+#                 raise ValueError("Series number {!s} erroneously appears at least twice!".format(k))
+#             dict_of_all_saved_manga_ids[k] = max_pages
+
+
 
 
 
